646f161eb8e4c48738d62af607212f78b22d72bf
Volkan Sendemir <38820790+volkansendemir@users.noreply.github.com>
2020-05-31 18:47:07 +0300

    Create Main.py

---
+++ 2020/FinalProject/babacan/Main.py
@@ -0,0 +1,11 @@
+from SentimentAnalysisBabacan.Download import download
+from SentimentAnalysisBabacan.CleanUp import clean_up
+from SentimentAnalysisBabacan.RandomPick import pick_random
+
+path = "C:/Users/vsendemir/Desktop/Koc/2020Spring/QMBU450/babacan/"
+#download(path)
+#clean_up(path)
+#pick_random(path=path, multiplier=3)
+#the labeling process is manual
+
+
