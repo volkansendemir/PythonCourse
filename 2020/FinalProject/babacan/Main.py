a77e90800ee39128fb280c71ee1b7393b85b23ad
Volkan Sendemir <38820790+volkansendemir@users.noreply.github.com>
2020-06-03 04:18:37 +0300

    Update Main.py

--- 2020/FinalProject/babacan/Main.py
+++ 2020/FinalProject/babacan/Main.py
@@ -1,11 +1,16 @@
 from SentimentAnalysisBabacan.Download import download
 from SentimentAnalysisBabacan.CleanUp import clean_up
 from SentimentAnalysisBabacan.RandomPick import pick_random
+from SentimentAnalysisBabacan.Analyze import analyze
+from SentimentAnalysisBabacan.Predict import predict
+from SentimentAnalysisBabacan.Present import present
 
 path = "C:/Users/vsendemir/Desktop/Koc/2020Spring/QMBU450/babacan/"
+
 #download(path)
 #clean_up(path)
-#pick_random(path=path, multiplier=3)
+#pick_random(path=path, multiplier=3) #sample size = multiplier * 250
 #the labeling process is manual
-
-
+analyze(path, num_rep=10)#LRC and tfidf performed somewhat better
+predict(path)
+present(path)
